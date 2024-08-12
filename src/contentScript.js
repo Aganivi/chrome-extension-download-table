1bbfa694d7a95c7b4fa5bd6ebc594a803e1abb09
Agan <Agan@AganMac.local>
2024-08-12 17:21:53 +0800

    fix a bug of first row in table

--- src/contentScript.js
+++ src/contentScript.js
@@ -5,35 +5,52 @@ const tables = document.body.querySelectorAll('.mytable');
 
 if (tables) {
   tables.forEach((table) => {
-    console.log(`table1:`, table);
-    let parent = table.parentNode;
-    let search = parent.querySelector('.range');
-
-    const download = document.createElement('div');
-    download.setAttribute('id', 'download-table');
-    download.setAttribute('class', 'search-btn');
-    download.style.marginLeft = '10px';
-
-    const button = document.createElement('button');
-    button.setAttribute('id', 'download-btn');
-    button.setAttribute('class', 'jzlyClass');
-    button.setAttribute('type', 'button');
-    button.innerText = '下载';
-    download.appendChild(button);
+    const search = table.parentNode.querySelector('.range');
+    const fileTitle = getTableTitle(search);
+    const download = createDownloadBtn();
     search.append(download);
 
-    // Package and Release Data (`writeFile` tries to write and save an XLSB file)
-    // XLSX.writeFile(workbook, 'Report.xlsx');
+    // Ensure that at least two tr elements exist.
+    // Otherwise, an error will occur in the first row of the table
+    const thead = table.querySelector('thead');
+    if (thead.children.length <= 1)
+      thead.appendChild(document.createElement('tr'));
+
     download.addEventListener('click', function () {
       let table_elt = table.querySelector('table');
-      let workbook = XLSX.utils.table_to_book(table_elt);
-      console.log(`workbook:`, workbook);
-      var ws = workbook.Sheets['Sheet1'];
-      console.log(`ws:`, ws);
-      XLSX.utils.sheet_add_aoa(ws, [['Created ' + new Date().toISOString()]], {
-        origin: -1,
-      });
-      XLSX.writeFile(workbook, 'Report.xlsx');
+      table_elt && downloadHTMLTableAsXLSX(table_elt, fileTitle);
     });
   });
+}
+
+function createDownloadBtn() {
+  const download = document.createElement('div');
+  download.setAttribute('id', 'download-table');
+  download.setAttribute('class', 'search-btn');
+  download.style.marginLeft = '10px';
+
+  const button = document.createElement('button');
+  button.setAttribute('id', 'download-btn');
+  button.setAttribute('class', 'jzlyClass');
+  button.setAttribute('type', 'button');
+  button.innerText = '下载';
+  download.appendChild(button);
+
+  return download;
+}
+
+function getTableTitle(el) {
+  const titleEl = el.closest('.in-content').querySelector('.inContent-title');
+  return titleEl ? titleEl.innerText : 'Table';
+}
+
+function downloadHTMLTableAsXLSX(table_elt, title) {
+  let workbook = XLSX.utils.table_to_book(table_elt);
+  console.log(`workbook:`, workbook);
+  var ws = workbook.Sheets['Sheet1'];
+  console.log(`ws:`, ws);
+  XLSX.utils.sheet_add_aoa(ws, [['Created ' + new Date().toISOString()]], {
+    origin: -1,
+  });
+  XLSX.writeFile(workbook, `${title}.xlsx`);
 }
