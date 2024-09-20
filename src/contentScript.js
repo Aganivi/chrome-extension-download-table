670b31e6e1b4f53f26303ee5b8cc453bacb8b9d6
Agan <1571698188@qq.com>
2024-09-20 21:51:39 +0800

    use hover icon to replace download btn

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,26 +1,34 @@
 'use strict';
 import * as XLSX from 'xlsx/xlsx.mjs';
+import axios from 'axios';
 
-const tables = document.body.querySelectorAll('.mytable');
+const tables = document.body.querySelectorAll('table');
 if (tables) {
   tables.forEach((table) => {
-    const t = table.querySelector('table');
+    const t = table;
     t.style.width = '100%';
+    t.style.position = 'relative';
 
     const search = table.parentNode.querySelector('.range');
-    const fileTitle = getTableTitle(search);
+    const fileTitle = search ? getTableTitle(search) : 'Table';
     const download = createDownloadBtn();
-    search.append(download);
+    t.append(download);
 
     // Ensure that at least two tr elements exist.
     // Otherwise, an error will occur in the first row of the table
     const thead = table.querySelector('thead');
-    if (thead.children.length <= 1)
+    if (thead && thead.children.length <= 1)
       thead.appendChild(document.createElement('tr'));
 
     download.addEventListener('click', function () {
-      let table_elt = table.querySelector('table');
-      table_elt && downloadHTMLTableAsXLSX(table_elt, fileTitle);
+      table && downloadHTMLTableAsXLSX(table, fileTitle);
+    });
+
+    t.addEventListener('mouseenter', function () {
+      download.style.display = 'flex';
+    });
+    t.addEventListener('mouseleave', function () {
+      download.style.display = 'none';
     });
   });
 }
@@ -29,14 +37,24 @@ function createDownloadBtn() {
   const download = document.createElement('div');
   download.setAttribute('id', 'download-table');
   download.setAttribute('class', 'search-btn');
-  download.style.marginLeft = '10px';
+  download.style.display = 'none';
+  download.style.position = 'absolute';
+  download.style.right = '10px';
+  download.style.top = '10px';
+  download.style.justifyContent = 'center';
+  download.style.alignItems = 'center';
+  download.style.padding = '2px';
+  download.style.width = '38px';
+  download.style.height = '38px';
+  download.style.cursor = 'pointer';
+  download.style.backgroundColor = '#fff';
+  download.style.border = '1px solid #ccc';
+  download.style.borderRadius = '50%';
 
-  const button = document.createElement('button');
-  button.setAttribute('id', 'download-btn');
-  button.setAttribute('class', 'jzlyClass');
-  button.setAttribute('type', 'button');
-  button.innerText = '下载';
-  download.appendChild(button);
+  const image = document.createElement('img');
+  image.setAttribute('id', 'download-btn');
+  image.src = chrome.runtime.getURL('icons/icon_32.png');
+  download.appendChild(image);
 
   return download;
 }
@@ -46,11 +64,27 @@ function getTableTitle(el) {
   return titleEl ? titleEl.innerText : 'Table';
 }
 
-function downloadHTMLTableAsXLSX(table_elt, title) {
-  let workbook = XLSX.utils.table_to_book(table_elt);
-  var ws = workbook.Sheets['Sheet1'];
+async function downloadHTMLTableAsXLSX(table_elt, title) {
+  let workbook = XLSX.utils.table_to_book(table_elt, { raw: true });
+  let ws = workbook.Sheets['Sheet1'];
   XLSX.utils.sheet_add_aoa(ws, [['Created ' + new Date().toISOString()]], {
     origin: -1,
   });
   XLSX.writeFile(workbook, `${title}.xlsx`);
+  // feishu 导入
+  // const tableValues = XLSX.utils.sheet_to_json(ws, { header: 1 });
+  // const res = await importTableIntoFeishu(title, tableValues);
 }
+
+const importTableIntoFeishu = async (tableName, tableValues) => {
+  const config = {
+    method: 'POST',
+    url: 'https://s0h4nj.laf.dev/feishu.js',
+    data: {
+      tableName,
+      tableValues,
+    },
+  };
+  const res = await axios(config);
+  return res.data;
+};
